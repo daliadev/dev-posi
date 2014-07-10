00785a160b188b3f6346aaa1132b4588148c3d45
daliadev <dalia.educfor.dev@gmail.com>
2014-07-10 14:27:21 +0200

    Note de version dans le footer : v 0.15.06

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,5 +1,5 @@
 
     <div id="footer">
-        <div id="txt-footer">© Education et formation <?php echo date("Y"); ?> &nbsp;- &nbsp;v0.15.05</div> 
+        <div id="txt-footer">© Education et formation <?php echo date("Y"); ?> &nbsp;- &nbsp;v0.15.06</div> 
         <!-- <p>Ceci est une version en cours de développement de l'application.<br/>Certaines informations et contenus peuvent être amenés à changer ou à disparaître.</p> -->
     </div>

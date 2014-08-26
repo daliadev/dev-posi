30c5a4d70521ed503e3ef866f1604960e39c49b4
daliadev <dalia.educfor.dev@gmail.com>
2014-08-26 10:26:34 +0200

    Modification du footer
    - Changement de version
    - La date devient statique

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,4 +1,5 @@
 
     <div id="footer">
-        <div id="txt-footer">© Education et formation <?php echo date("Y"); ?> &nbsp;- &nbsp;v0.16.03</div> 
+        <!-- <div id="txt-footer">© Education et formation <?php //echo date("Y"); ?> &nbsp;- &nbsp;v0.16.04</div> -->
+        <div id="txt-footer">© Education et formation 2014 &nbsp;- &nbsp;v0.16.04</div> 
     </div>

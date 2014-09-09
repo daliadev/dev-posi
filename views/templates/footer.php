4ba856739c61ebb9e7da19e6ed9aa58d18db6003
daliadev <dalia.educfor.dev@gmail.com>
2014-09-09 14:55:11 +0200

    Modification du footer
    - Changement de version -> v0.16.06
    - L'année redevient dynamique

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,5 +1,4 @@
 
     <div id="footer">
-        <!-- <div id="txt-footer">© Education et formation <?php //echo date("Y"); ?> &nbsp;- &nbsp;v0.16.04</div> -->
-        <div id="txt-footer">© Education et formation 2014 &nbsp;- &nbsp;v0.16.05</div> 
+        <div id="txt-footer">© Education et formation <?php echo date("Y"); ?> &nbsp;- &nbsp;v0.16.06</div>
     </div>

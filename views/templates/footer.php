38bc4f406b150463cfd8a8b9e7c618d645aa9836
daliadev <dalia.educfor.dev@gmail.com>
2014-09-22 10:31:07 +0200

    Changement de version dans le footer v0.16.09

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,4 +1,4 @@
 
     <div id="footer">
-        <div id="txt-footer">© Education et formation <?php echo date("Y"); ?> &nbsp;- &nbsp;v0.16.08</div>
+        <div id="txt-footer">© Education et formation <?php echo date("Y"); ?> &nbsp;- &nbsp;v0.16.09</div>
     </div>

35822415cd508483f87a6d7971dd80a83fb939bc
daliadev <dalia.educfor.dev@gmail.com>
2015-03-25 16:15:37 +0100

    Ajout de fichiers templates-head pour la mise en place d'un nouveau système de templates

---
+++ views/templates/head_default.php
@@ -0,0 +1,28 @@
+<head>
+    
+    <meta charset="UTF-8">
+    <title>Positionnement DALIA</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <link type="image/x-icon" rel="shortcut icon" href="<?php echo SERVER_URL; ?>favicon.ico" />
+
+    <link type="text/css" rel="stylesheet" href="<?php echo SERVER_URL; ?>media/css/hot-sneaks/jquery-ui-1.10.3.custom.css" />
+    <link type="text/css" rel="stylesheet" href="<?php echo SERVER_URL; ?>media/css/reset.css" />
+    <link type="text/css" rel="stylesheet" media="all" href="<?php echo SERVER_URL; ?>media/css/layout.css" />
+    <link type="text/css" rel="stylesheet" media="all" href="<?php echo SERVER_URL; ?>media/css/480.css" />
+
+    <link type="text/css" rel="stylesheet" href="<?php echo SERVER_URL; ?>media/projekktor/themes/dalia/projekktor-dalia.style.css" media="screen" />
+    
+    <!--[if lt IE 9]>
+    <script src="<?php echo SERVER_URL; ?>media/js/dist/html5shiv.js"></script>
+    <![endif]-->
+    
+    <script src="<?php echo SERVER_URL; ?>media/js/jquery-1.11.2.min.js" type="text/javascript"></script>
+    <script src="<?php echo SERVER_URL; ?>media/js/jquery-ui-1.10.3.custom.all.js" type="text/javascript"></script>
+    <!--<script src="<?php //echo SERVER_URL; ?>media/js/lightbox-2.6.min.js" type="text/javascript"></script>
+    <script src="<?php //echo SERVER_URL; ?>media/js/jquery.tablesorter.js" type="text/javascript"></script>-->
+    <script src="<?php echo SERVER_URL; ?>media/projekktor/projekktor-1.3.09.min.js" type="text/javascript"></script>
+    
+</head>
+  

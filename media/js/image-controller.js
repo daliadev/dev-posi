55243e97957e49959f915d1232ffc9b70024d5f9
Nicohubb <n.beurion@gmail.com>
2015-06-15 22:52:24 +0200

    Aménagement des animations du chargement de l'image

--- media/js/image-controller.js
+++ media/js/image-controller.js
@@ -4,36 +4,124 @@ var ImageController = function(container, loader) {
 	var self = this;
 	var container = container;
 	var loader = loader;
-	var onLoadFunction = null;
+	//var onLoadFunction = null;
 	var imageBox = new Image();
 	var blackBg = null; //$('#black-bg');
 
-	this.startLoading = function(imgSrc, loaderFadeDuration, onLoadCallback) {
+	var loadTimer = null;
+	var loaderFadeDuration = 0;
+	var loaderInterval = 40;
+	var loaderCallback = null;
 
-		onLoadFunction = onLoadCallback;
+	var displayTimer = null;
+	var displayFadeDuration = 0;
+	var displayInterval = 40;
+	var displayCallback = null;
 
-		imageBox.onload = function() {
-			
-			imageBox.style.display = 'none';
-			container.prepend(imageBox);
-			//self.container.css('height', 'auto');
-			//self.container.css('padding-bottom', '0px');
-			loader.fadeOut(loaderFadeDuration);
 
-			if (typeof onLoadFunction === 'function') {
-				onLoadFunction.call(self);
-			}
-		};
 
-		imageBox.src = imgSrc;
-		loader.fadeIn(loaderFadeDuration);
+	var loaderFadeIn = function() {
+		
+		var opacity = loader.style.opacity;
+		opacity += (1000 / loaderInterval) / loaderFadeDuration;
+
+		if (opacity >= 1) {
+
+			clearInterval(loadTimer);
+			loader.style.opacity = 1;
+			loaderCallback.call(this, 'fadeIn');
+		}
+	};
+
+	var loaderFadeOut = function() {
+
+		var opacity = loader.style.opacity;
+		opacity -= (1000 / loaderInterval) / loaderFadeDuration;
+
+		if (opacity <= 0) {
+
+			clearInterval(loadTimer);
+			loader.style.display = 'none';
+			loader.style.opacity = 1;
+			loaderCallback.call(this, 'fadeOut');
+		}
+	}
+
+
+	var onDisplay = function() {
+
+		var opacity = imageBox.style.opacity;
+		opacity += (1000 / displayInterval) / displayFadeDuration;
+
+		if (opacity >= 1) {
+
+			clearInterval(displayTimer);
+			imageBox.style.opacity = 1;
+			displayCallback.call(this);
+		}
 	};
 
 
-	this.display = function(duration) {
 
-		$(imageBox).fadeIn(duration);
-	}
+
+	this.startLoading = function(imgSrc, loaderDuration, onLoadCallback) {
+
+		loaderFadeDuration = loaderDuration;
+		onLoadFunction = onLoadCallback;
+
+		loader.style.opacity = 0;
+		loader.style.display = 'block';
+		loadTimer = setInterval(loaderFadeIn, loaderInterval);
+
+		imageBox.onload = function() {
+			
+			loader.style.opacity = 1;
+			loader.style.display = 'block';
+			displayTimer = setInterval(onDisplay, displayInterval);
+
+			imageBox.style.display = 'none';
+			//container.prepend(imageBox);
+			//self.container.css('height', 'auto');
+			//self.container.css('padding-bottom', '0px');
+			//loader.fadeOut(loaderFadeDuration);
+			//loadTimer = setInterval(onLoad, loaderInterval);
+
+		};
+
+		imageBox.src = imgSrc;
+		//loader.fadeIn(loaderFadeDuration);
+	};
+
+	this.setLoaderInterval(duration) {
+
+		loaderInterval = duration;
+	};
+	this.getLoaderInterval() {
+
+		return loaderInterval;
+	};
+
+
+	this.display = function(duration, onDisplayCallback) {
+
+		//$(imageBox).fadeIn(duration);
+		displayFadeDuration = duration;
+		displayCallback = onDisplayCallback;
+		imageBox.style.opacity = 0;
+		imageBox.style.display = 'block';
+		displayTimer = setInterval(onDisplay, displayInterval);
+	};
+
+	this.setDisplayInterval(duration) {
+
+		displayInterval = duration;
+	};
+	this.getDisplayInterval() {
+
+		return displayInterval;
+	};
+
+
 
 
 	this.fadeToBlack = function(element, duration) {
@@ -51,6 +139,6 @@ var ImageController = function(container, loader) {
 			this.blackBg.fadeOut(500);
 			this.blackBg.remove();
 		}
-	}
+	};
 
 }

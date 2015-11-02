c08dd29abf31cc729915fd7662cc2d1e807e88c7
nico-educfor <n.beurion@education-et-formation.fr>
2015-11-02 16:52:17 +0100

    Dev classe ArraySort

--- utils/array_sort.php
+++ utils/array_sort.php
@@ -54,12 +54,112 @@ class ArraySort {
 	}
 
 
-
-	static public function recursiveArray($index, $depth, $datas, $currentArray = array())
+	/**
+	 *
+	 *	Algorythme Arbre binaire (en pseudo C ou C) issu de 'Exercices et problèmes d'algorythmique:
+	 * 
+	 * 	// Définition structure d'un noeud
+	 * 	typedef struct arbre
+	 * 	{
+	 * 		T info;
+	 * 		struct arbre *sag; // Pointeur sous-arbre gauche
+	 * 		struct arbre *sad; // Pointeur sous-arbre droite
+	 * 	} arbre;
+	 * 	typedef arbre *ptr_arbre; // Définition pointeur arbre départ
+	 * 
+	 *	- Création
+	 *	- Parcourir (en profondeur) :
+	 * 		FONCTION preordre(a : ptr_arbre)
+	 * 		DEBUT
+	 * 			SI a <> NULL ALORS
+	 * 				afficher(a->info)
+	 * 				preordre(a->sag)
+	 * 				preordre(a->sad)
+	 * 			FINSI
+	 * 		FIN
+	 * 
+	 * 		FONCTION ordre(a : ptr_arbre)
+	 * 		DEBUT
+	 * 			SI a <> NULL ALORS
+	 * 				ordre(a->sag)
+	 * 				afficher(a->info)
+	 * 				ordre(a->sad)
+	 * 			FINSI
+	 * 		FIN
+	 * 
+	 * 		FONCTION postordre(a : ptr_arbre)
+	 * 		DEBUT
+	 * 			SI a <> NULL ALORS
+	 * 				postordre(a->sag)
+	 * 				postordre(a->sad)
+	 * 				afficher(a->info)
+	 * 				
+	 * 			FINSI
+	 * 		FIN
+	 *
+	 *	- Rechercher :
+	 * 		FONCTION recherche(x : T, a : ptr_arbre) : booléen
+	 * 		VAR ok : booléen
+	 * 		DEBUT
+	 * 			SI a = NULL ALORS
+	 * 				ok<-faux
+	 * 			SINON SI a->info = x ALORS
+	 * 				ok<-vrai
+	 * 			SINON SI a->info > x ALORS
+	 * 				recherche(x, a->sag, ok)
+	 * 			SINON
+	 * 				recherche(x, a->sad, ok)
+	 * 			FINSI
+	 * 			RETOURNER ok
+	 * 		FIN
+	 * 	
+	 * 	- Ajouter :
+	 * 		FONCTION ajout(x : T, *aj_a : ptr_arbre) : vide
+	 * 		DEBUT
+	 * 			SI *aj_a = NULL ALORS
+	 * 				reserver(*aj_a)
+	 * 				*aj_a->info <- x
+	 * 				*aj_a->sag <- NULL
+	 * 				*aj_a->sad <- NULL
+	 * 			SINON SI *aj_a->info <= x ALORS
+	 * 				ajout(x, &(*aj_a->sad))
+	 * 			SINON
+	 * 				ajout(x, &(*aj_a->sag))
+	 * 			FINSI
+	 * 			RETOURNER ok
+	 * 		FIN
+	 * 
+	 * 	- Compter :
+	 * 		FONCTION compter(a : ptr_arbre) : entier
+	 * 		VAR n : entier
+	 * 		DEBUT
+	 * 			SI a = NULL ALORS
+	 * 				n <- 0
+	 * 			SINON
+	 * 				n <- 1 + compter(a->sag) + compter(a->sad)
+	 * 			FINSI
+	 * 			RETOURNER n
+	 * 		FIN
+	 * 
+	 * 	- Hauteur :
+	 * 		FONCTION hauteur(a : ptr_arbre) : entier
+	 * 		VAR n : entier
+	 * 		DEBUT
+	 * 			SI a = NULL ALORS
+	 * 				n <- 0
+	 * 			SINON
+	 * 				n <- 1 + maximum(compter(a->sag), compter(a->sad))
+	 * 			FINSI
+	 * 			RETOURNER n
+	 * 		FIN
+	 *	
+	 */
+	
+	static public function recursiveArray($parentRef, $depth, $datasObjects, $refField)
     {
     	//var_dump($depth, 'yo', $index, 'end');
     	$array = null;
-    	$tempArray = array();
+    	//$tempArray = array();
 		$previousDepth = 0;
 		//$count = 0;
 
@@ -67,20 +167,25 @@ class ArraySort {
 		{
 			$array = array();
 		}
+		/*
 		else
 		{
 			$array = $currentArray;
 		}
+		*/
 
 		$i = 0;
 
-		foreach ($datas as $node) 
+		foreach ($datasObjects as $object) 
 		{
-			//var_dump($count);
-			if ($depth - 1 == count($array[$index]))
+			if ($parentRef == $object[$refField])
 			{
-				$index++;
-				//var_dump(count($currentArray));
+				//$index++;
+
+				if ($previousDepth < $depth) 
+				{
+					$list .= '<ul>';
+				}
 
 				for ($i = 0; $i < count($depth); $i++) 
 				{ 	
@@ -91,7 +196,7 @@ class ArraySort {
 				//array_push($tempArray, $node);
 				$previousDepth = $depth;
 
-				$array[$depth] = self::recursiveArray($index, ($depth + 1), $datas, $tempArray);
+				$array[$depth] = self::recursiveArray($object[$refField], ($depth + 1), $datasObjects, $refField);
 			}
 
 			$i++;

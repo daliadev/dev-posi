eebc523605bbba8d8474c88e82dae37e6d4ce9dd
nico-educfor <n.beurion@education-et-formation.fr>
2015-11-02 13:29:20 +0100

    Changement des tables concernant les préconisations de parcours
    - suppression table cat_preco -> les infos sont rassemblées dans la table preconisation
    - Supression table intervalle -> également au profit de la table préconisation
    - Mise à jour / création des models correspondants
    
    Signed-off-by: nico-educfor <n.beurion@education-et-formation.fr>

--- models/preconisation.php
+++ models/preconisation.php
@@ -4,47 +4,59 @@
 
 class Preconisation
 {
-    
-    public $id_preco = NULL;
-    public $ref_parcours = NULL;
-    public $ref_intervalle = NULL;
-    public $nom_preco = NULL;
-    public $descript_preco = NULL;
-    public $num_ordre = NULL;
+	
+	public $id_preco = NULL;
+	public $ref_code_cat = NULL;
+	public $ref_parcours = NULL;
+	// public $ref_inter = NULL;
+	public $nom_preco = NULL;
+	public $descript_preco = NULL;
+	public $taux_min = NULL;
+	public $taux_max = NULL;
+	public $num_ordre = NULL;
 
+	
+	public function getId()
+	{
+		return $this->id_preco;
+	}
+	
+	public function getRefParcours()
+	{
+		return $this->ref_parcours;
+	}
 
-    
-    public function getId()
-    {
-        return $this->id_preco;
-    }
-    
-    public function getRefParcours()
-    {
-        return $this->ref_parcours;
-    }
+	// public function getRefInter()
+	// {
+	//     return $this->ref_inter;
+	// }
+	
+	public function getNom()
+	{
+		return $this->nom_preco;
+	}
+	
+	public function getDescription()
+	{
+		return $this->descript_preco;
+	}
 
-    public function getRefIntervalle()
-    {
-        return $this->ref_intervalle;
-    }
-    
-    public function getNom()
-    {
-        return $this->nom_preco;
-    }
-    
-    public function getDescription()
-    {
-        return $this->descript_preco;
-    }
-    
-    public function getNumOrdre()
-    {
-        return $this->num_ordre;
-    }
+	public function getTauxMin()
+	{
+		return $this->taux_min;
+	}
 
-    
+	public function getTauxMax()
+	{
+		return $this->taux_max;
+	}
+	
+	public function getNumOrdre()
+	{
+		return $this->num_ordre;
+	}
+
+	
 }
 
 ?>

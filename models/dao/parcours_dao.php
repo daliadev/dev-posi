0adc4903d77e27c24c08eb026836f9c4b75c3b2e
nico-educfor <n.beurion@education-et-formation.fr>
2015-11-04 16:47:31 +0100

    Ajout préconisation DAO

---
+++ models/dao/parcours_dao.php
@@ -0,0 +1,153 @@
+<?php
+
+
+// Inclusion du fichier de la classe Parcours
+require_once(ROOT.'models/parcours.php');
+
+
+
+class ParcoursDAO extends ModelDAO
+{
+
+	
+	/**
+	 * selectAll - Retourne la liste de tous les parcours.
+	 * 
+	 * @return array Liste d'objets "Parcours".
+	 */
+	public function selectAll() 
+	{
+		$this->initialize();
+
+		$request = "SELECT * FROM parcours ORDER BY nom_parcours ASC";
+		
+		$this->resultset['response'] = $this->executeRequest("select", $request, "parcours", "Parcours");
+
+		return $this->resultset;
+	}
+
+	
+	
+	
+	
+	/**
+	 * selectById - Récupère le parcours correspondant à l'identifiant.
+	 * 
+	 * @param int Identifiant du parcours.
+	 * @return array parcours correspondant à l'identifiant sinon erreurs.
+	 */
+	public function selectById($idDegre) 
+	{
+		$this->initialize();
+		
+		if (!empty($idDegre))
+		{
+			$request = "SELECT * FROM parcours WHERE id_parcours = ".$idDegre;
+
+			$this->resultset['response'] = $this->executeRequest("select", $request, "parcours", "Parcours");
+		}
+		else
+		{
+			$this->resultset['response']['errors'][] = array('type' => "form_request", 'message' => "Les données sont vides");
+		}
+		
+		return $this->resultset;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * insert - Insère un parcours
+	 * 
+	 * @param array Valeurs du parcours à inserer
+	 * @return bool Vrai si l'insertion a fonctionné
+	 */
+	public function insert($values) 
+	{
+	   $this->initialize();
+		
+		if (!empty($values))
+		{       
+			$request = $this->createQueryString("insert", $values, "parcours");
+			
+			$this->resultset['response'] = $this->executeRequest("insert", $request, "parcours", "Parcours");
+		}
+		else
+		{
+			$this->resultset['response']['errors'][] = array('type' => "insert", 'message' => "Les données sont vides");
+		}
+		
+		return $this->resultset;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * update - Met à jour un parcours
+	 * 
+	 * @param array Valeurs du parcours à mettre à jour
+	 * @return array Nbre de lignes mises à jour sinon erreurs
+	 */
+	public function update($values) 
+	{
+		$this->initialize();
+		
+		if (!empty($values))
+		{
+			if (isset($values['ref_parcours']) && !empty($values['ref_parcours']))
+			{
+				$refParcours = $values['ref_parcours'];
+				unset($values['ref_parcours']);
+				
+				$request = $this->createQueryString("update", $values, "parcours", "WHERE id_parcours = ".$refParcours);
+				
+				$this->resultset['response'] = $this->executeRequest("update", $request, "parcours", "Parcours");
+			}
+			else
+			{
+				$this->resultset['response']['errors'][] = array('type' => "update", 'message' => "Il n'y a aucun identifiant pour le parcours à mettre à jour.");
+			}
+		}
+		else
+		{
+			$this->resultset['response']['errors'][] = array('type' => "update", 'message' => "Les données sont vides");
+		}
+
+		return $this->resultset;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * delete - Efface un parcours
+	 * 
+	 * @param int Identifiant du parcours
+	 * @return array Nbre de lignes effacées sinon erreurs
+	 */
+	public function delete($refParcours) 
+	{
+		$this->initialize();
+		
+		if (!empty($refParcours))
+		{
+			$request = "DELETE FROM degre WHERE id_parcours = ".$refParcours;
+
+			$this->resultset['response'] = $this->executeRequest("delete", $request, "parcours", "Parcours");
+		}
+		else
+		{
+			$this->resultset['response']['errors'][] = array('type' => "delete", 'message' => "Il n'y a aucun identifiant pour la suppression du parcours.");
+		}
+
+		return $this->resultset;
+	}
+	
+}
+
+?>
---
+++ models/dao/preconisation_dao.php
@@ -0,0 +1,176 @@
+<?php
+
+
+
+// Inclusion du fichier de la classe Categorie
+require_once(ROOT.'models/preconisation.php');
+
+
+
+class preconisationDAO extends ModelDAO
+{
+
+    
+    
+    /**
+     * selectAll - Retourne la liste de toutes les catégories
+     * 
+     * @return array Liste d'objets "Categorie"
+     */
+    public function selectAll() 
+    {
+        $this->initialize();
+
+        $request = "SELECT * FROM question_cat ORDER BY code_cat ASC";
+        
+        $this->resultset['response'] = $this->executeRequest("select", $request, "question_cat", "QuestionCategorie");
+
+        return $this->resultset;
+    }
+    
+    
+    
+    
+    
+    /**
+     * selectByCode - Récupère la catégorie correspondant au code.
+     * 
+     * @param string Code de la catégorie
+     * @return array Catégorie correspondant au code sinon erreurs
+     */
+    public function selectByCodeCat($codeCat) 
+    {
+        $this->initialize();
+        
+        if (!empty($codeCat))
+        {
+            $request = "SELECT * FROM question_cat WHERE ref_cat = ".$codeCat;
+
+            $this->resultset['response'] = $this->executeRequest("select", $request, "question_cat", "QuestionCategorie");
+        }
+        else
+        {
+            $this->resultset['response']['errors'][] = array('type' => "select", 'message' => "Il n'y a aucun code pour la catégorie recherchée.");
+        }
+        
+        return $this->resultset;
+    }
+    
+    
+    public function selectByRefQuestion($refQuestion) 
+    {
+        $this->initialize();
+        
+        if (!empty($refQuestion))
+        {
+            $request = "SELECT * FROM question_cat WHERE ref_question = ".$refQuestion;
+
+            $this->resultset['response'] = $this->executeRequest("select", $request, "question_cat", "QuestionCategorie");
+        }
+        else
+        {
+            $this->resultset['response']['errors'][] = array('type' => "select", 'message' => "Il n'y a aucun code pour la catégorie recherchée.");
+        }
+        
+        return $this->resultset;
+    }
+
+    
+    
+    
+    /**
+     * insert - Insère une catégorie
+     * 
+     * @param array Valeurs de la catégorie à inserer
+     * @return bool Vrai si l'insertion a fonctionné
+     */
+    public function insert($values) 
+    { 
+        $this->initialize();
+        
+        if (isset($values['ref_question']) && !empty($values['ref_question']) && isset($values['ref_cat']) && !empty($values['ref_cat']))
+        {
+            $request = "INSERT INTO question_cat (ref_question, ref_cat) VALUES (".$values['ref_question'].", '".$values['ref_cat']."')";
+
+            $this->resultset['response'] = $this->executeRequest("insert", $request, "question_cat", "QuestionCategorie");
+        }
+        else
+        {
+            $this->resultset['response']['errors'][] = array('type' => "insert", 'message' => "Les données sont vides");
+        }
+            
+        return $this->resultset;
+    }
+    
+    
+    
+    
+    
+    /**
+     * update - Met à jour une catégorie
+     * 
+     * @param array Valeurs de la catégorie à mettre à jour
+     * @return array Nbre de lignes mises à jour sinon erreurs
+     */
+    public function update($values) 
+    {
+        $this->initialize();
+        
+       
+        
+        if (!empty($values))
+        {
+            if (isset($values['ref_question']) && !empty($values['ref_question']) && isset($values['ref_cat']) && !empty($values['ref_cat']))
+            {
+                $refQuestion = $values['ref_question'];
+                unset($values['ref_question']);
+                
+                $request = $this->createQueryString("update", $values, "question_cat", "WHERE ref_question = ".$refQuestion);
+                
+                $this->resultset['response'] = $this->executeRequest("update", $request, "question_cat", "QuestionCategorie");
+            }
+            else
+            {
+                $this->resultset['response']['errors'][] = array('type' => "update", 'message' => "Il n'y a aucun identifiant pour la question et la catégorie à mettre à jour.");
+            }
+        }
+        else
+        {
+            $this->resultset['response']['errors'][] = array('type' => "update", 'message' => "Les données sont vides");
+        }
+
+        return $this->resultset;
+    }
+    
+    
+    
+    
+    
+    /**
+     * delete - Efface une catégorie
+     * 
+     * @param int Identifiant de la question
+     * @return array Nbre de lignes effacées sinon erreurs
+     */
+    public function delete($refQuestion) 
+    {
+        $this->initialize();
+        
+        if (!empty($refQuestion))
+        {
+            $request = "DELETE FROM question_cat WHERE ref_question = ".$refQuestion;
+
+            $this->resultset['response'] = $this->executeRequest("delete", $request, "question_cat", "QuestionCategorie");
+        }
+        else
+        {
+            $this->resultset['response']['errors'][] = array('type' => "delete", 'message' => "Il n'y a aucun identifiant pour la suppression de la catégorie.");
+        }
+
+        return $this->resultset;
+    }
+    
+
+}
+
+?>

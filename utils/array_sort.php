820e46faff235d25b46ea159e682ba2e19daf5da
nico-educfor <n.beurion@education-et-formation.fr>
2015-11-06 11:39:24 +0100

    Finition classe ArraySort

--- utils/array_sort.php
+++ utils/array_sort.php
@@ -57,138 +57,35 @@ class ArraySort {
 	
 	static public function recursiveArray($parentRef, $depth, $datasObjects, $refField, $refCharIncrement = 1)
     {
-    	//var_dump($depth, 'yo', $index, 'end');
+
     	$array = null;
-    	
-    	//$tempArray = array();
-		$prevDepth = 0;
-		//$count = 0;
 
 		if ($depth == 0) 
 		{
 			$array = array();
 		}
-		/*
-		else
-		{
-			$array = $currentArray;
-		}
-		*/
-
 		
-
-		/*
-		foreach ($datasObjects as $object) 
-		{
-			if ($parentRef == $object[$refField])
-			{
-				//$index++;
-
-				if ($previousDepth < $depth) 
-				{
-					$list .= '<ul>';
-				}
-
-				for ($i = 0; $i < count($depth); $i++) 
-				{ 	
-					//$tempArray = 
-				}
-				
-				$array[] = $tempArray;
-				//array_push($tempArray, $node);
-				$previousDepth = $depth;
-
-				$array[$depth] = self::recursiveArray($object[$refField], ($depth + 1), $datasObjects, $refField);
-			}
-
-			//$i++;
-		}
-		*/
-
-		//$i = 0;
-
 
 		foreach ($datasObjects as $key => $object) 
 		{
-			//var_dump($key, $object);
 			$objectParentRef = null;
-
-			//if ($parentRef !== null)
-			//{
-				$objectParentRef = substr($object->$refField, 0, (strlen($object->$refField) - $refCharIncrement));
-			//}
-			/*
-			else
-			{
-				$objectParentRef = $object->$refField;
-			}
-			*/
-			
-			//$parentCode = $object[$refField]
-			//var_dump($objectParentRef);
+			$objectParentRef = substr($object->$refField, 0, (strlen($object->$refField) - $refCharIncrement));
 
 			if ($parentRef == $objectParentRef)
 			{
-				//var_dump($depth, $parentRef, $objectParentRef, $object);
-				
-				//$tempArray = null;
 				$iterativeResult = null;
-
-				//$i = 0;
-
-				if ($prevDepth < $depth) 
-				{
-					//$tempArray = array();
-					//$array[] = array();
-				}
-				/*
-				if ($tempArray !== null)
-				{
-					$tempArray[$i] =  $object;
-				}
-				else
-				{
-					$tempArray =  $object;
-				}
-				*/
-				/*
-				for ($i = 0; $i < $depth; $i++) { 
-					# code...
-				}
-				*/
-				//$prevDepth = $depth;
 
 				$array[] = $object;
 				
-				$array[] = self::recursiveArray($object->$refField, ($depth + 1), $datasObjects, $refField, $refCharIncrement);
+				$iterativeResult = self::recursiveArray($object->$refField, ($depth + 1), $datasObjects, $refField, $refCharIncrement);
 
-				/*
-				if ($iterativeResult !== null)
+
+				if ($iterativeResult !== null) 
 				{
-					$tempArray[$i] = array($iterativeResult);
+					$array[] = $iterativeResult;
 				}
-
-				/*
-				if ($array !== null && $tempArray !== null) 
-				{
-					$array[] = $tempArray;
-				}
-
-				var_dump($array, $depth);
-				exit;
-				$i++;
-				*/
 			}
 		}
-		/*
-		if ($prevDepth == $depth && $prevDepth != 0) 
-		{
-			//$index++;
-			//$array[] = $tempArray;
-		}
-		*/
-
-		//var_dump($array);
 
 		return $array;
     }

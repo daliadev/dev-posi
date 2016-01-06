49d7120a4a42d058ea2e91f858d6e919b2d768c0
nico-perso <n.beurion.dev@gmail.com>
2016-01-07 00:52:43 +0100

    Page résultat : animation du cercle de pourcentage global

--- media/js/pages/resultat.js
+++ media/js/pages/resultat.js
@@ -1,6 +1,55 @@
 
-//$(function() { 
+$(function() { 
+
+	var animCircle = null;
+	var animbar = null;
+
+	var percentGlobal = 0;
+	
+	var circleCircum = 0;
+	var circleOffset = 0;
+	var circleOffsetTarget = 0;
+
+	var progressBars = new Array();
+
+	var barsLength = new Array();
+
+
+	function animateCircle() {
+
+		d = circleOffset - circleOffsetTarget;
+		circleOffset -= d * 0.05;
+
+		$('#circle-percent').css('stroke-dashoffset', circleOffset);
+
+		if (circleOffset <= circleOffsetTarget) {
+
+			clearInterval(animCircle);
+		}
+	}
+
+
+	function animateBar() {
+
+		
+	}
+
+
+
+	var reponsesCorrectes = new Number($('#reponses-ok').html());
+	var nbreReponses = new Number($('#reponses-global').html());
+	percentGlobal = Math.round(reponsesCorrectes / nbreReponses * 100);
+
+	var circleCircumValue = $('#circle-percent').css('stroke-dasharray');
+	circleCircum = circleCircumValue.substring(0, circleCircumValue.length - 2)
+	$('#circle-percent').css('stroke-dashoffset', circleCircum);
+
+	circleOffsetTarget = Math.round(circleCircum - (circleCircum / 100 * percentGlobal));
+	circleOffset = circleCircum;
+
+	animCircle = setInterval(animateCircle, 40);
+
+	//animbar = setInterval(animateBar, 40);
 
 	
-	
-//});
+});

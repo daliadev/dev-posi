e72d1cf6c22dad2bd90d41c7d5ac4ba486601338
nico-educfor <n.beurion@education-et-formation.fr>
2016-04-21 16:54:51 +0200

    Copie fichiers template dans menu principal

---
+++ views/templates/footer.php
@@ -0,0 +1,20 @@
+		
+		<div class="footer" id="footer">
+			<p><a href="#">A propos </a> - &nbsp;© Education et formation 2014-<?php echo date("Y"); ?></p>
+			<p>v<?php echo Config::POSI_VERSION; ?></p>
+		</div>
+
+
+		
+		<!-- <script type="text/javascript" src="<?php //echo SERVER_URL; ?>media/js/navigator-agent.js"></script>
+		<script type="text/javascript"> -->
+		
+		<!-- var navAgent = new NavigatorAgent();
+
+		var footerTxt = document.getElementById('footer').innerHTML;
+		//footerTxt += '<p>Navigateur : ' + navAgent.getUserAgent() + '</p>';
+		footerTxt += '<p>' + navAgent.getName() + ' -  ' + navAgent.getVersion() + '</p>';
+		document.getElementById('footer').innerHTML = footerTxt;
+		
+
+		</script> -->

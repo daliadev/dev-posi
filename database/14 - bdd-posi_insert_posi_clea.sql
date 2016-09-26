3fc3f038621fe86f566488a3ac6231fba9549138
nico-educfor <n.beurion@education-et-formation.fr>
2016-09-26 16:31:04 +0200

    Seed table positionnement

---
+++ database/14 - bdd-posi_insert_posi_clea.sql
@@ -0,0 +1,10 @@
+
+
+/* Insertion de la liste des niveaux d'études */
+
+INSERT INTO positionnement (id_posi, nom_posi, lien_posi, descript_posi)
+	VALUES 	(1, "Domaine 1", "http://positionnement.educationetformation.fr/clea/domaine1/", NULL),
+		(2, "Domaine 2", "http://positionnement.educationetformation.fr/clea/domaine2/", NULL),
+		(3, "Domaine 3", "http://positionnement.educationetformation.fr/clea/domaine3/", NULL),
+		(4, "Domaine 7", "http://positionnement.educationetformation.fr/clea/domaine7/", NULL);
+
